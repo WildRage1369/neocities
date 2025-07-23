2eaad47575e70a4ff4990454e7d7367df7f3279a
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2025-07-23 15:32:53 -0400

    Feat: change INode to be just a struct and move INode functions elsewhere and fix Init function for fs

--- lib/fs.zig
+++ lib/fs.zig
@@ -15,10 +15,11 @@ const FileOpenError = error{
 /// file data. It is designed to be used in a WebAssembly environment.
 /// It is required to call .destroy() on it to free the memory
 pub const FileSystemTree = struct {
-    root: *INode,
+    inodes_list: std.ArrayList(INode),
+    root: usize,
     file_data_map: std.AutoHashMap(usize, []const u8),
     fd_table: std.AutoHashMap(usize, u64), // file descriptor -> serial number
-    serial_number_counter: u16,
+    serial_number_counter: usize,
     allocator: std.mem.Allocator,
 
     /// Creates a new FileSystemTree (owned by the caller) and initializes it with the
@@ -31,60 +32,46 @@ pub const FileSystemTree = struct {
         this.* = .{
             .allocator = allocator,
             .file_data_map = std.AutoHashMap(usize, []const u8).init(allocator),
+            .inodes_list = std.ArrayList(INode).init(allocator),
             .fd_table = std.AutoHashMap(usize, u64).init(allocator),
             .serial_number_counter = 0,
-            .root = try INode.create(.{
-                .allocator = this.allocator,
-                .name = "/",
-                .serial_number = this.getSerialNum(),
-                .file_type = FileType.directory,
-            }),
+            .root = this.getSerialNum(),
         };
+
+        try this.inodes_list.append(INode.create(.{
+            .allocator = this.allocator,
+            .name = "/",
+            .serial_number = this.getSerialNum(),
+            .file_type = FileType.directory,
+        }));
 
         // fill fd_table with stdin, stdout, stderr
         // stdin, stdout, and stderr are floating INodes, only accessable by FD
-        const stdin = try INode.create(.{
-            .allocator = this.allocator,
-            .name = "stdin",
-            .serial_number = this.getSerialNum(),
-            .file_type = FileType.character_device,
-        });
-        try this.fd_table.put(0, stdin.serial_number);
-
-        const stdout = try INode.create(.{
-            .allocator = this.allocator,
-            .name = "stdin",
-            .serial_number = this.getSerialNum(),
-            .file_type = FileType.character_device,
-        });
-        try this.fd_table.put(1, stdout.serial_number);
-
-        const stderr = try INode.create(.{
-            .allocator = this.allocator,
-            .name = "stdin",
-            .serial_number = this.getSerialNum(),
-            .file_type = FileType.character_device,
-        });
-        try this.fd_table.put(2, stderr.serial_number);
+        const outputs = [_][]const u8{"stdin", "stdout", "stderr"};
+        for (outputs, 0..) |output, i| {
+            const node = INode.create(.{
+                .allocator = this.allocator,
+                .name = output,
+                .serial_number = this.getSerialNum(),
+                .file_type = FileType.character_device,
+            });
+            try this.inodes_list.append(node);
+            try this.fd_table.put(i, node.serial_number);
+        }
 
         // create base directories
-        var opts: INode.CreateArgs = .{
-            .name = "tmp",
-            .serial_number = this.getSerialNum(),
-            .file_type = FileType.directory,
-            .parent = this.root,
-            .allocator = this.allocator,
-        };
-        try this.root.addChildINode(try INode.create(opts));
-        opts.name = "home";
-        opts.serial_number = this.getSerialNum();
-        try this.root.addChildINode(try INode.create(opts));
-        opts.name = "bin";
-        opts.serial_number = this.getSerialNum();
-        try this.root.addChildINode(try INode.create(opts));
-        opts.name = "dev";
-        opts.serial_number = this.getSerialNum();
-        try this.root.addChildINode(try INode.create(opts));
+        const children = [_][]const u8{"tmp", "home", "bin", "dev"};
+        for (children) |child| {
+            const node = INode.create(.{
+                .name = child,
+                .serial_number = this.getSerialNum(),
+                .file_type = FileType.directory,
+                .parent = this.root,
+                .allocator = this.allocator,
+            });
+            try this.inodes_list.append(node);
+            try this.addChildINode(this.root, node.serial_number);
+        }
 
         return this;
     }
@@ -176,10 +163,9 @@ pub const FileSystemTree = struct {
         return data.len;
     }
 
-
     /// @return returns the INode of the file located at file_path
     /// @param file_path: string
-    pub fn getINode(self: *FileSystemTree, file_path: []const u8) FileOpenError!*INode {
+    fn getINode(self: *FileSystemTree, file_path: []const u8) FileOpenError!*INode {
         var current_node = self.root;
         var input_path_itr = std.mem.splitScalar(u8, file_path, '/');
         _ = input_path_itr.first(); // skip first element
@@ -210,6 +196,32 @@ pub const FileSystemTree = struct {
         self.allocator.destroy(self);
     }
 
+    // ---------- INode Functions ----------
+
+    fn get(self: *FileSystemTree, serial_number: usize) *INode {
+        if (serial_number > self.inodes_list.items.len) {
+            std.debug.panic("FileSystemTree.get() failed, serial_number > inodes_list.items.len ({d} > {d})\n", .{ serial_number, self.inodes_list.items.len });
+        }
+        return &self.inodes_list.items[serial_number];
+    }
+
+    /// @brief Add a child INode to this INode and update input INode. Ownership of input INode is transferred to this INode
+    /// @param child: INode to add to this INode's children and to be owned by this INode
+    pub fn addChildINode(self: *FileSystemTree, parent: usize, child: usize) !void {
+        try self.get(parent).children.append(child);
+        self.get(child).parent = parent;
+    }
+
+    /// @brief Add a list of children INodes to this INode
+    /// @param new_children: ArrayList of INodes to add to this INode's children
+    pub fn addChildArrayList(self: *FileSystemTree, parent: usize, new_children: *std.ArrayList(usize)) !void {
+        const child_slice = try new_children.toOwnedSlice();
+        try self.get(parent).children.appendSlice(child_slice);
+        for (child_slice) |child| {
+            self.get(child).parent = parent;
+        }
+    }
+
     // ---------- Private Functions ----------
 
     /// @brief Creates a file with the given path and data type
@@ -235,7 +247,10 @@ pub const FileSystemTree = struct {
     }
 
     /// @returns serial number pre-incremented
-    fn getSerialNum(self: *FileSystemTree) u64 {
+    fn getSerialNum(self: *FileSystemTree) usize {
+        if (self.serial_number_counter != self.inodes_list.items.len) {
+            std.debug.panic("Invariant violated, serial_number_counter != inodes_list.items.len ({d} != {d})\n", .{ self.serial_number_counter, self.inodes_list.items.len });
+        }
         defer self.serial_number_counter += 1;
         return self.serial_number_counter;
     }
@@ -300,3 +315,51 @@ test "write to already existing file with EXCL flag" {
     _ = try fs.writeByPath("/tmp/test.txt", .{ .CREAT = true }, "Hello There", .string);
     try std.testing.expectError(FileOpenError.AccessDenied, fs.writeByPath("/tmp/test.txt", .{ .EXCL = true }, "Hello There", .string));
 }
+
+// INode tests
+
+// test "addChildINode" {
+//     const allocator = std.testing.allocator;
+//
+//     const inode = try INode.create(.{
+//         .allocator = allocator,
+//         .name = "test",
+//         .serial_number = 1,
+//         .file_type = FileType.directory,
+//         .timestamp = Timestamp.currentTime(),
+//     });
+//
+//     // do NOT defer as ownership is transferred to parent INode
+//     const child = try INode.create(.{
+//         .allocator = allocator,
+//         .name = "child",
+//         .serial_number = 2,
+//         .file_type = FileType.directory,
+//         .timestamp = Timestamp.currentTime(),
+//     });
+//
+//     try inode.addChildINode(child);
+// }
+//
+// test "changeParent" {
+//     const allocator = std.testing.allocator;
+//
+//     // do NOT defer as ownership is transferred to parent INode
+//     const inode = try INode.create(.{
+//         .allocator = allocator,
+//         .name = "test",
+//         .serial_number = 1,
+//         .file_type = FileType.directory,
+//         .timestamp = Timestamp.currentTime(),
+//     });
+//
+//     const parent = try INode.create(.{
+//         .allocator = allocator,
+//         .name = "parent",
+//         .serial_number = 1,
+//         .file_type = FileType.directory,
+//         .timestamp = Timestamp.currentTime(),
+//     });
+//
+//     try inode.changeParent(parent);
+// }
